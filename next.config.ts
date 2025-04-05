adb04c1a5da6268deadb8a07591baceb72135a89
Yves Roland Ouiya <ouiyayves@gmail.com>
2025-04-05 07:37:57 +0000

    Redirection SEO

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,20 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // ✅ Ignore les erreurs ESLint au build
+    ignoreDuringBuilds: true, // ✅ Ignore les erreurs ESLint bloquantes au build
   },
-};
-
-module.exports = {
   images: {
     domains: ['labelshop-backend.onrender.com', 'labr1.odoo.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/shop/:slug',       // 🔁 ancienne structure
+        destination: '/products/:slug', // ✅ nouvelle structure
+        permanent: true,             // 🔁 redirection 301
+      },
+    ];
+  },
 };
-
 
 export default nextConfig;
